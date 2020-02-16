d80fc4abc37896096c05e397f0e8ee9cfc65da21
Andrew Butler-Boudakian <andrew.butlerboudakian@keycaptureenergy.com>
2020-02-16 17:40:15 -0500

    Created Blank QT Project

---
+++ QT GUI Demo/build-FutureWalletDemo-Desktop_x86_windows_msvc2015_pe_32bit-Debug/CMakeFiles/FutureWalletDemo.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FutureWalletDemo.dir/FutureWalletDemo_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/FutureWalletDemo.dir/main.cpp.obj"
+  "CMakeFiles/FutureWalletDemo.dir/mainwindow.cpp.obj"
+  "CMakeFiles/FutureWalletDemo_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/FutureWalletDemo_autogen.dir/ParseCache.txt"
+  "FutureWalletDemo_autogen"
+  "FutureWalletDemo.exe"
+  "FutureWalletDemo.exe.manifest"
+  "FutureWalletDemo.lib"
+  "FutureWalletDemo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FutureWalletDemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
