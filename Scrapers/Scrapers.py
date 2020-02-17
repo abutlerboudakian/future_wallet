d52597c205e1d7336940af37f1c981b2208ef4e9
Andrew Butler-Boudakian <andrew.butlerboudakian@keycaptureenergy.com>
2020-02-16 20:40:25 -0500

    Created Housing Index Scraper

--- Scrapers/Scrapers.py
+++ Scrapers/Scrapers.py
@@ -17,8 +17,13 @@ def parseZillowRentData(uri):
 	page = requests.get(uri)
 	return pd.read_csv(io.StringIO(page.text))
 
+def parseHousingIndexData(uri):
+	page = requests.get(uri)
+	return pd.read_csv(io.StringIO(page.text))
+
 if __name__ == "__main__":
-	#TBdata = parseTBondData('https://www.firstrepublic.com/finmkts/historical-interest-rates')
-	ZRdata = parseZillowRentData('http://files.zillowstatic.com/research/public/Zip/Zip_Zri_AllHomesPlusMultifamily.csv')
-	print(ZRdata)
+	# TBdata = parseTBondData('https://www.firstrepublic.com/finmkts/historical-interest-rates')
+	# ZRdata = parseZillowRentData('http://files.zillowstatic.com/research/public/Zip/Zip_Zri_AllHomesPlusMultifamily.csv')
+	HIdata = parseHousingIndexData('https://www.fhfa.gov/HPI_master.csv')
+	print(HIdata)
 	
