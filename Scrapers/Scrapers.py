9a44859315a370c3da25cc77cfc8b14282d1669e
Andrew Butler-Boudakian <andrew.butlerboudakian@keycaptureenergy.com>
2020-02-17 20:15:29 -0500

    Completed parseSAData

--- Scrapers/Scrapers.py
+++ Scrapers/Scrapers.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 # from arron import *
 import requests
-# import bs4
+from bs4 import BeautifulSoup
 import io
 import pandas as pd
 
@@ -20,19 +20,35 @@ def parseTBondData(uri, driver):
 	driver.get(uri)
 	tableData = driver.find_element_by_id('GraphTable')
 	table = tableData.get_attribute('outerHTML')
-	driver.quit()
 	return pd.read_html(table)
 
-def parseSAData(uri, driver):
+def parseSAData(uri, root, driver, depth=0):
+	njdf = pd.DataFrame()
+	jdf = pd.DataFrame()
 	driver.get(uri)
 	div = driver.find_element_by_id('content')
-	soup = BeautifulSoup(div.get_attribute('outerHTML'))
+	soup = BeautifulSoup(div.get_attribute('outerHTML'), 'lxml')
 	tables = soup.find_all('table')
 	if len(tables) > 0:
-		
-	else:
+		njdf = pd.read_html(str(tables[0]))[0]
+		jdf = pd.read_html(str(tables[1]))[0]
+		njdf['Date'] = pd.to_datetime(((uri.split('/'))[-1]).split('.')[0], format='%Y-%m-%d')
+		jdf['Date'] = pd.to_datetime(((uri.split('/'))[-1]).split('.')[0], format='%Y-%m-%d')
+	elif depth <= 1:
 		ul = soup.find_all('ul')[0]
-		items = ul.
+		items = ul.find_all('li')
+		for item in items:
+			link = root + item.find('a')['href']
+			(nj, j) = parseSAData(link, root, driver, depth+1)
+			if njdf.empty:
+				njdf = pd.DataFrame(nj)
+			else:
+				njdf = njdf.append(nj)
+			if jdf.empty:
+				jdf = pd.DataFrame(j)
+			else:
+				jdf = jdf.append(j)
+	return (njdf, jdf)
 
 
 def parseCpiData(uri, driver):
@@ -41,7 +57,6 @@ def parseCpiData(uri, driver):
 	driver.find_element_by_xpath("//input[@id='dv-submit']").click()
 	tableData = driver.find_element_by_id('seriesDataTable1')
 	table = tableData.get_attribute('outerHTML')
-	driver.quit()
 	return pd.read_html(table)
 
 def parseCsvData(uri):
@@ -77,9 +92,10 @@ if __name__ == "__main__":
 	# CDdata = parseCDData('https://fred.stlouisfed.org/graph/fredgraph.csv?id=CD6NRJD')
 	# BondData = parseBondData('https://datahub.io/core/bond-yields-us-10y/r/monthly.csv')
 	# IncomeData = parseCBPIncomeData("https://www2.census.gov/programs-surveys/cbp/datasets/2017/cbp17cd.xlsx?#")
-	CpiData = parseCpiData('https://beta.bls.gov/dataViewer/view/timeseries/CUSR0000SA0')
-	SAdata = parseSAData('https://www.fdic.gov/regulations/resources/rates/previous.html', driver)
+	# CpiData = parseCpiData('https://beta.bls.gov/dataViewer/view/timeseries/CUSR0000SA0')
+	SAdata = parseSAData('https://www.fdic.gov/regulations/resources/rates/previous.html', 'https://www.fdic.gov/regulations/resources/rates/', driver)
+	print(SAdata)
 	# RaremetalData = parseRaremetalData('https://datahub.io/core/gold-prices/r/monthly.csv')
 	# print(CpiData)
-	stockData = parseStockData("https://dividata.com/")
-	
+	# stockData = parseStockData("https://dividata.com/")
+	driver.quit()
