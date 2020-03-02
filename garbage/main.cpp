03a4db4f0306ef9a4dd7a10618bc0e33def398b1
arronlook <33300146+arronlook@users.noreply.github.com>
2020-03-01 22:58:55 -0500

    Modified Garbage in personal test branch to play with Qt

--- garbage/main.cpp
+++ garbage/main.cpp
@@ -1,11 +1,12 @@
 #include "mainwindow.h"
 
-#include <QApplication>
+#include <QApplication> // Handles event handling and mouse movements, etc.
+#include <QDebug> // For debugging output
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
-    return a.exec();
+    w.showMaximized();
+    return a.exec(); // Puts application into loop for diff events in application to be handled
 }
