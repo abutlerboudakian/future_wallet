436dc846d5a156d0f9572c22e289148ea1e8b2eb
Andrew Butler-Boudakian <andrew.butlerboudakian@keycaptureenergy.com>
2020-03-03 20:23:50 -0500

    Full model skeleton, all TF wrapper stubs present

--- Model/models.py
+++ Model/models.py
@@ -20,6 +20,9 @@ class BaseModel(ABC):
 	def load(self, models):
 		pass
 
+	def predict(self, income, lat, lon, years):
+		pass
+
 class WageModel(BaseModel):
 	def __init__(self, data=None):
 		pass
@@ -40,12 +43,13 @@ class InvestmentModel(BaseModel):
 
 	def predict(self, savings, cd, stocks, bonds, tbs, years):
 		stockRet = 0
+		timestamp = datetime.now().timestamp()
 		predTimestamp = (datetime.now() + timedelta(years=years)).timestamp()
 		for ticker, stock, shares in stocks:
-			predStock = stockModels[ticker].predict(predTimestamp, stock)
+			predStock = stockModels[ticker].predict(timestamp, predTimestamp, stock)
 			stockRet += ((predStock - stock)*shares)
 		stockRet /= years
-		return stockRet + savingsModel.predict(predTimestamp, savings) + cdModel.predict(predTimestamp, cd) + bondModel.predict(predTimestamp, bonds) + tbModel.predict(predTimestamp, tbs)
+		return stockRet + savingsModel.predict(timestamp, predTimestamp, savings) + cdModel.predict(timestamp, predTimestamp, cd) + bondModel.predict(timestamp, predTimestamp, bonds) + tbModel.predict(timestamp, predTimestamp, tbs)
 		
 
 class AssetModel(BaseModel):
@@ -61,35 +65,92 @@ class AssetModel(BaseModel):
 	def predict(self, residences, rents, rm, years):
 		resRet = 0
 		rentRet = 0
+		timestamp = datetime.now().timestamp()
 		predTimestamp = (datetime.now() + timedelta(years=years)).timestamp()
-		for res in residences:
-			predRes = residenceModel.predict(predTimestamp, res)
+		for (res, lat, lon) in residences:
+			predRes = residenceModel.predict(timestamp, predTimestamp, res, lat, lon)
 			resRet += (predRes - res)/years
-		for rent in rents:
-			predRent = rentModel.predict(predTimestamp, rent)
+		for (rent, lat, lon) in rents:
+			predRent = rentModel.predict(timestamp, predTimestamp, rent, lat, lon)
 			rentRet += predRent*12
-		return resRet + rentRet + rmModel.predict(predTimestamp, rm)
+		return resRet + rentRet + rmModel.predict(timestamp, predTimestamp, rm)
 
 class SavingsModel(BaseModel):
-	pass
+	def __init__(self, data=None):
+		pass
+
+	def load(self, model):
+		pass
+
+	def predict(self, timestamp, predTimestamp, value):
+		pass
 
 class CDModel(BaseModel):
-	pass
+	def __init__(self, data=None):
+		pass
+
+	def load(self, model):
+		pass
+
+	def predict(self, timestamp, predTimestamp, value):
+		pass
 
 class StockModel(BaseModel):
-	pass
+	def __init__(self, data=None):
+		pass
+
+	def load(self, model):
+		pass
+
+	def predict(self, timestamp, predTimestamp, value):
+		pass
 
 class BondModel(BaseModel):
-	pass
+	def __init__(self, data=None):
+		pass
+
+	def load(self, model):
+		pass
+
+	def predict(self, timestamp, predTimestamp, value):
+		pass
 
 class TBModel(BaseModel):
-	pass
+	def __init__(self, data=None):
+		pass
+
+	def load(self, model):
+		pass
+
+	def predict(self, timestamp, predTimestamp, value):
+		pass
 
 class ResidenceModel(BaseModel):
-	pass
+	def __init__(self, data=None):
+		pass
+
+	def load(self, model):
+		pass
+
+	def predict(self, timestamp, predTimestamp, value, lat, long):
+		pass
 
 class RentModel(BaseModel):
-	pass
+	def __init__(self, data=None):
+		pass
+
+	def load(self, model):
+		pass
+
+	def predict(self, timestamp, predTimestamp, value, lat, long):
+		pass
 
 class RMModel(BaseModel):
-	pass
+	def __init__(self, data=None):
+		pass
+
+	def load(self, model):
+		pass
+
+	def predict(self, timestamp, predTimestamp, value):
+		pass
