626d4b6363918b20baed20e7157630223b627ea7
Andrew Butler-Boudakian <andrew.butlerboudakian@keycaptureenergy.com>
2020-03-03 20:04:10 -0500

    More work on models, no TF code yet

--- Model/modelfactory.py
+++ Model/modelfactory.py
@@ -10,13 +10,13 @@ class ModelFactory:
 	def __init__(self):
 		dsb = DatasetBuilder()
 
-	def createModel(self, mType):
+	def createModel(self, mType, args):
 		if mType == ModelType.WAGES:
-			model = WageModel(dsb.getModelData(ModelType.WAGES))
+			model = WageModel(dsb.getModelData(ModelType.WAGES, args))
 		else if mType == ModelType.INVESTS:
-			model = InvestmentModel(dsb.getModelData(ModelType.INVESTS))
+			model = InvestmentModel(dsb.getModelData(ModelType.INVESTS, args))
 		else if mType == ModelType.ASSETS:
-			model = AssetModel(dsb.getModelData(ModelType.ASSETS))
+			model = AssetModel(dsb.getModelData(ModelType.ASSETS, args))
 		else:
 			raise 'Invalid model type specified.', mType
 
--- Model/models.py
+++ Model/models.py
@@ -1,5 +1,6 @@
 from abc import ABC
 from enum import Enum
+from datetime import datetime, timedelta
 
 class ModelType(Enum):
 	WAGES = 1
@@ -7,13 +8,82 @@ class ModelType(Enum):
 	ASSETS = 3
 
 class BaseModel(ABC):
-	pass
+	def __init__(self, data=None):
+		self.data = data
+
+	def load(self, models):
+		pass
 
 class WageModel(BaseModel):
-	pass
+	def __init__(self, data=None):
+		pass
 
 class InvestmentModel(BaseModel):
-	pass
+	def __init__(self, data=None):
+		savingsData, cdData, stockData, bondData, tbData = zip(*data)
+		self.savingsModel = SavingsModel(savingsData)
+		self.cdModel = CDModel(savingsData)
+		self.stockModels = []
+		for s in stockData:
+			self.stockModels[s['ticker']] = StockModel(s)
+		self.bondModel = BondModel(bondData)
+		self.tbModel = TBModel(tbData)
+
+	def load(self, models):
+		pass
+
+	def predict(self, savings, cd, stocks, bonds, tbs, years):
+		stockRet = 0
+		predTimestamp = (datetime.now() + timedelta(years=years)).timestamp()
+		for ticker, stock, shares in stocks:
+			predStock = stockModels[ticker].predict(predTimestamp, stock)
+			stockRet += ((predStock - stock)*shares)
+		stockRet /= years
+		return stockRet + savingsModel.predict(predTimestamp, savings) + cdModel.predict(predTimestamp, cd) + bondModel.predict(predTimestamp, bonds) + tbModel.predict(predTimestamp, tbs)
+		
 
 class AssetModel(BaseModel):
+	def __init__(self, data=None):
+		resData, rentData, rmData = zip(*data)
+		self.residenceModel = ResidenceModel(resData)
+		self.rentModel = RentModel(rentData)
+		self.rmModel = RMModel(rmData)
+
+	def load(self, models):
+		pass
+
+	def predict(self, residences, rents, rm, years):
+		resRet = 0
+		rentRet = 0
+		predTimestamp = (datetime.now() + timedelta(years=years)).timestamp()
+		for res in residences:
+			predRes = residenceModel.predict(predTimestamp, res)
+			resRet += (predRes - res)/years
+		for rent in rents:
+			predRent = rentModel.predict(predTimestamp, rent)
+			rentRet += predRent*12
+		return resRet + rentRet + rmModel.predict(predTimestamp, rm)
+
+class SavingsModel(BaseModel):
+	pass
+
+class CDModel(BaseModel):
+	pass
+
+class StockModel(BaseModel):
+	pass
+
+class BondModel(BaseModel):
+	pass
+
+class TBModel(BaseModel):
+	pass
+
+class ResidenceModel(BaseModel):
+	pass
+
+class RentModel(BaseModel):
+	pass
+
+class RMModel(BaseModel):
 	pass
