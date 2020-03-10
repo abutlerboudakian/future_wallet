66973335fec6c43e9af540855f53be3108e7e4ac
arronlook <33300146+arronlook@users.noreply.github.com>
2020-03-10 10:34:24 -0400

    slight update

--- Application/Application/main.cpp
+++ Application/Application/main.cpp
@@ -1,11 +1,12 @@
 #include "mainapplication.h"
+#include <memory>
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainApplication w;
-    w.show();
+    std::unique_ptr<MainApplication> w (new MainApplication);
+    w->show();
     return a.exec();
 }
