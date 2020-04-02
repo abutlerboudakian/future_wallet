2fbf732e2f5b90565af8f727abc49493ab714da2
arronlook <33300146+arronlook@users.noreply.github.com>
2020-04-02 18:35:36 -0400

    Current implementation of controller done

--- Application/Application/Controller.cpp
+++ Application/Application/Controller.cpp
@@ -49,6 +49,34 @@ void switchToLogin()
 //-------------------------------------
 // Charts                             |
 //-------------------------------------
-QChartView * getPieChart(ChartMap * data);
-QChartView * getBarGraph(ChartMap * data);
-QChartView * getLineGraph(LineMap * data);
+/* Creates a pie chart for the given data
+ * @requires data to be of the form DataName->PercentDecimal for every pair
+ * @param data is a hashmap of std::string->double for DataName->PercentDecimal
+ * @returns a chartview of the pie chart, representing the given data
+ */
+QChartView * getPieChart(ChartMap * data)
+{
+  return PieCreator->make(data);
+}
+
+/* Creates a bar graph for the given data
+ * @requires data to be a ChartMap with std::string->double denoting BarName->Value pairs
+ * @param data is a hashmap of std::string->double for BarName->Value
+ * @returns a chartview of the pie chart, representing the given data
+ */
+QChartView * getBarGraph(ChartMap * data)
+{
+  return BarCreator->make(data);
+}
+
+/* Creates a line graph for the given data
+ * @requires data to be an unorderedmap of std::string->std::vector<std::pair<QDateTime, double> >
+ *           where std::string is the line name and std::vector contains points to graph
+ *           X Axis is QDateTime, Y is double
+ * @param data is an unordered map, representing the lines we need to graph
+ * @returns a chartview of the line graph, representing the given data
+ */
+QChartView * getLineGraph(LineMap * data)
+{
+  return LineCreator->make(data);
+}
