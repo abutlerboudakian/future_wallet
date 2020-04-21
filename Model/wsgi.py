ad5e53000a428e5b470d75e0fbe0c251701e47a9
Andrew Butler-Boudakian <andrew.butlerboudakian@keycaptureenergy.com>
2020-04-20 21:05:29 -0400

    Added Comments, got updated API file from Flask server

--- Model/wsgi.py
+++ Model/wsgi.py
@@ -1,4 +1,12 @@
-from api import app as application
+# Import statements
+import os
+from dotenv import load_dotenv
+from api import app
 
+# Load server virtual environment into Python path
+proj_folder = os.path.expanduser('~/futurewallet')
+load_dotenv(os.path.join(proj_folder, '.env'))
+
+# Main WSGI driver block
 if __name__ == '__main__':
-	application.run()
+    app.run()
